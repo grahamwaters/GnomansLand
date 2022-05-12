202cad3aca1b1942e1b4ea328fd9a803db0bbe2e
Graham Waters <graham.waters.business@gmail.com>
2022-05-12 16:05:32 -0500

    adds some boilerplate items.

---
+++ src/gnome_basic.py
@@ -0,0 +1,7 @@
+'''
+Doc string
+'''
+
+demo_variable_imported_from_module: str = "Hello! I come from the module 'module_example.py'"
+
+gnome_name: str = "Jerome"
---
+++ src/package_example/__init__.py
@@ -0,0 +1,3 @@
+'''Docstring'''
+
+demo_variable_imported_from_package: str = "Hello! I come from the package 'package_example.py'"
